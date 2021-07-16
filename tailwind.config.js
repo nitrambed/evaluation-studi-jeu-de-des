97f821495f78f2a2479beeec6f9610134c0e4778
Nitrambed <deb.martin@live.fr>
2021-07-16 19:19:51 +0200

    Integrating css in html markup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        'lato': ['"Lato"', 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
